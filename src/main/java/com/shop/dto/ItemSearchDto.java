14c86d1e8c803a9e86d84eee159927373b1e0126
kimjjing1004 <kimjjing1004@naver.com>
2024-11-14 03:28:07 +0900

    6.3 상품 관리하기 개발완료

---
+++ src/main/java/com/shop/dto/ItemSearchDto.java
@@ -0,0 +1,17 @@
+package com.shop.dto;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Data;
+
+@Data
+public class ItemSearchDto {
+
+    private String searchDateType;
+
+    private ItemSellStatus searchSellStatus;
+
+    private String searchBy;
+
+    private String searchQuery = "";
+
+}
